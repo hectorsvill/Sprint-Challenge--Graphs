069431d678d67ef450d09a6ecfd7fb224dbf2bec
hectorsvill <hectorstevenvillasano@gmail.com>
2019-11-07 16:42:36 -0800

    map 0

--- graph_adventure/path.py
+++ graph_adventure/path.py
@@ -15,38 +15,69 @@ class Queue():
     def size(self):
         return len(self.queue)
 
+
+class Stack():
+    def __init__(self):
+        self.stack = []
+    def push(self, value):
+        self.stack.append(value)
+    def pop(self):
+        if self.size() > 0:
+            return self.stack.pop()
+        else:
+            return None
+    def size(self):
+        return len(self.stack)
+
+
+
+
 class Path:
     def __init__(self, player):
         self.player = player
 
     def getPath(self):
-        pass
-    
+        exits = self.player.currentRoom.getExits()
+        
+        mapped = dict()
+        mapped[self.player.currentRoom.id] = {'n': "?", 's': "?",'e': "?",'w': "?", }
+
+
+        for e in exits:
+            mapped[self.player.currentRoom.id][e] = self.player.currentRoom.getRoomInDirection(e).id
+
+
+
+
+        print(mapped)
     def get_map(self):
 
         self.player.travel("n")
 
         # currentRoom = self.player.currentRoom
         # roomid = currentRoom.id
-        # exits = self.player.currentRoom.getExits()
+        
         # coord = (currentRoom.x, currentRoom.y)
         # print(exits, coord, roomid)
-        
-        q = Queue()
+        path = []
+        s = Stack()
         visited = {}
         
-        q.enqueue(self.player.currentRoom.getExits())
-        while q.size() > 0:
+        s.push(self.player.currentRoom.getExits())
+        while s.size() > 0:
             
-            path = q.dequeue()
+            path = s.pop()
             v = path[-1]
-            
             if v not in visited:
                 self.player.travel(v)
+                path.append(v)
                 visited[v] = path
                 exits = self.player.currentRoom.getExits()
                 for e in exits:
                     path_copy = path.copy()
                     path_copy.append(e)
-                    q.enqueue(path_copy)
-        return visited
+                    s.push(path_copy)
+        return path
+
+
+
